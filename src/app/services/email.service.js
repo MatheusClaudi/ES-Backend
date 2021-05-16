6181d62ca7a2f51e6d1cdae2eae44042e0fa44cb
MatheusClaudi <matheus.claudino@ccc.ufcg.edu.br>
2021-05-16 11:24:25 -0300

    d

--- src/app/services/email.service.js
+++ src/app/services/email.service.js
@@ -20,7 +20,7 @@ const sendEmail = async (email, token) => {
                 <div style='width: 50%; margin: 0 25% 0 25%; background-color: white; padding: 50px; border-radius: 30px;'>
                     <h3 style='font-size: 22px; font-weight: normal; color: rgb(63, 67, 69); text-align: center; margin: 30px 30% 0 30%; width: 40%;'> Clique no botão abaixo para redefinir sua senha </h3>
                     <div style="display: flex; justify-content: center; width: 100%; text-aling: center;">
-                            <a href="${process.env.BASE_URL}/recoverPassword/${token}" style="display: flex; cursor: pointer; justify-content: center; margin-top: 50px; text-decoration: none; width: 100%; "> 
+                            <a href="http://localhost:4200/reset?token=${token}" style="display: flex; cursor: pointer; justify-content: center; margin-top: 50px; text-decoration: none; width: 100%; "> 
                                     <button style="width: 100%; padding: 20px; background-color: #32CD32; color: #FFFFFF; border: none; border-radius: 15px; font-size: 18px; cursor: pointer;"> 
                                             Redefinir senha 
                                     </button>
